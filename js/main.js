3c59efde6676620497ba0a03c8b772d84ac8a84d
Maksim <137396773+MaksimMaksimishin@users.noreply.github.com>
2024-02-07 17:41:20 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,81 +1,11 @@
-// // $('.content-item')
-// //     .on('click', ($this) => {
-// //         console.log($this);
-// //     })
-// //     .css('background: grey; color: white; font-size: 30px;');
 
-// // function $(selector) {
-// //     const items = document.querySelectorAll(selector);
-
-
-// //     const methods = {
-// //         on(userEvent, handler) {
-// //             items.forEach(item => {
-// //                 item.addEventListener(userEvent, (e) =>{
-// //                     handler(item, e);
-// //                 });
-// //             })
-
-// //         return methods;
-// //         },
-// //         css(cssText) {
-// //             items.forEach(item => {
-// //                 item.style.cssText = cssText;
-// //             });
-
-// //             return methods;
-// //         },
-// //     };
-// //     return methods;
-// // }
-
-
-// // // addHandler('content-item', 'click',() => {
-// // //     console.log('click');
-// // // });
-
-// // // addCss('content-item', 'background: grey; color: white;');
-
-
-// // // function addHandler(selector, userEvent, cbHandler) {
-// // //     const items = getEls(selector);
-
-// // //     items.forEach(item => {
-// // //        item.addEventListener(userEvent, cbHandler);
-// // //     });
-// // // }
-
-// // // function addCss(selector, cssText) {
-// // //     const items = getEls(selector);
-
-// // //     items.forEach(item => {
-// // //         item.style.cssText = cssText;
-// // //      });
-// // // }
-
-// // // function getEls(selector) {
-// // //     return document.querySelectorAll(selector);
-// // // }
-
-
-// $(function($) {
-//     $('.content-item').each((index, item) => {
-//         console.log(item);
-//     });
-
-//     $('.box').append('<div class="box-item">create el</div>');
-
-//     $('.content').click(($this) => {
-//         $('.content').toggle('slow');
-//     });
-// }); 
 
 
 $(document).ready(function () {
-    // Загрузка списка задач при загрузке страницы
+   
     loadTodos();
 
-    // Обработка формы добавления задачи
+    
     $('#todoForm').submit(function (event) {
         event.preventDefault();
         const todoText = $('#todoInput').val();
@@ -88,26 +18,26 @@ $(document).ready(function () {
         }
     });
 
-    // Обработка клика по задаче (отметка как выполненная/невыполненная)
+    
     $('#todoList').on('click', 'li', function () {
         const todoId = $(this).data('id');
         toggleTodoStatus(todoId);
     });
 
-    // Обработка клика по кнопке удаления задачи
+    
     $('#todoList').on('click', 'button.delete', function (event) {
-        event.stopPropagation(); // Предотвращение переключения статуса при клике на кнопку
+        event.stopPropagation(); 
         const todoId = $(this).closest('li').data('id');
         deleteTodo(todoId);
     });
 
-    // Обработка клика по кнопке фильтрации задач
+    
     $('#filterButtons').on('click', 'button', function () {
         const status = $(this).data('status');
         filterTodos(status);
     });
 
-    // Функция для загрузки задач из сервера
+    
     function loadTodos() {
         $.ajax({
             url: 'http://localhost:3000/todos',
@@ -118,7 +48,7 @@ $(document).ready(function () {
         });
     }
 
-    // Функция для добавления задачи на сервер
+    
     function addTodo(todo) {
         $.ajax({
             url: 'http://localhost:3000/todos',
@@ -131,7 +61,7 @@ $(document).ready(function () {
         });
     }
 
-    // Функция для переключения статуса задачи на сервере
+    
     function toggleTodoStatus(todoId) {
         $.ajax({
             url: `http://localhost:3000/todos/${todoId}`,
@@ -144,7 +74,7 @@ $(document).ready(function () {
         });
     }
 
-    // Функция для удаления задачи на сервере
+    
     function deleteTodo(todoId) {
         $.ajax({
             url: `http://localhost:3000/todos/${todoId}`,
@@ -155,7 +85,7 @@ $(document).ready(function () {
         });
     }
 
-    // Функция для отображения задач на странице
+    
     function displayTodos(todos) {
         $('#todoList').empty();
         todos.forEach(function (todo) {
@@ -169,7 +99,7 @@ $(document).ready(function () {
         });
     }
 
-    // Функция для фильтрации задач по статусу
+    
     function filterTodos(status) {
         $.ajax({
             url: `http://localhost:3000/todos?completed=${status}`,
